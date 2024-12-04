eeb1be1f99cca2f2662574638ae4a9f32dd5b52c
RedOctober117 <dbcahilly3@gmail.com>
2024-12-03 19:46:56 -0600

    script creation and eula

--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
 # import hashlib
-from optional import Nothing, Option, Optional, Something
 import requests
 from io import BytesIO
 from pathlib import Path
+import os
+from contextlib import chdir
 
-type Error = str
-type Result[T] = T | Error
+# type Error = Exception
+# type Result[T] = T | Error
 
 def main():
+    host_system_os = os.name
+
+    print('NOTE: By using this program, you agree to the Mojang EULA.')
     mojang_mani = get_mojang_manifest('https://gist.githubusercontent.com/cliffano/77a982a7503669c3e1acb0a0cf6127e9/raw/a36b1e2f05ed3f1d1bbe9a7cf7fd3cfc7cb7a3a8/minecraft-server-jar-downloads.md') 
     cursor_index = 0
-    global jar
-    jar = Nothing
     help_string = '\n\th -- Display this help screen\n\tj -- Scroll down\n\tk -- Scroll up\
         \n\tls -- list all\n\tq -- Exit program\n'
+    global jar_path
+    jar_path = None
 
     print(list_manifest_versions(mojang_mani, cursor_index, 5))
     print('Please select a version', end='')
     while(True):
-        print(': ', end='')
-        user_selection = input()
+        user_selection = input(': ')
         match user_selection:
             case 'j' | 'J': 
                 cursor_index = cursor_index + 1 if cursor_index < len(mojang_mani) else cursor_index
@@ -37,24 +40,44 @@ def main():
             case _: 
                 if user_selection in mojang_mani:
                     print(f'Selected {user_selection}. Downloading. . .')
-                    jar = Optional.of(download_jar(user_selection, mojang_mani[user_selection]))
+                    jar_path = download_jar(user_selection, mojang_mani[user_selection])
                     break
                 else:
-                    print(f'\nInvalid entry. Enter a valid version number or see the commands below: {help_string}')
+                    print(f'\nInvalid entry. Enter a valid version number or see the options below: {help_string}')
 
-def download_jar(version_name: str, url: str) -> Result[Path]:    
+    match input('Do you agree to Mojang\'s EULA? '):
+        case 'y' | 'Y':
+            with chdir(f'{jar_path.parent}'):
+                with open('eula.txt', 'w') as file:
+                    file.write('eula=true')
+                match host_system_os:
+                    case 'nt':
+                        with open('start.bat', 'w') as file:
+                            file.write(f'java -Xmx1024M -Xms1024M -jar {jar_path.name} nogui')
+                    case _:
+                        with open('start.sh', 'w') as file:
+                            file.write(f'java -Xmx1024M -Xms1024M -jar {jar_path.name} nogui')
+
+                os.system(f'java -Xmx1024M -Xms1024M -jar {jar_path.name} nogui')
+        case _:
+            print('EULA declined. Terminating. . .')
+            exit()
+
+
+
+def download_jar(version_name: str, url: str) -> Path:
     r = requests.get(url)
     container = Path('server_files')
     if not container.exists():
         container.mkdir()
     written_path = Path(container, f'{version_name}_server.jar')
-    try:
-        with open(written_path, 'wb') as file:
-            file.write(BytesIO(r.content).getbuffer())
-    except BlockingIOError as err:
-        return f'BlockingIOError: {err}'
-    except OSError as err:
-        return f'OSError: {err}'
+    # try:
+    with open(written_path, 'wb') as file:
+        file.write(BytesIO(r.content).getbuffer())
+    # except BlockingIOError as err:
+    #     return err
+    # except OSError as err:
+    #     return err
     
     return written_path
 
@@ -76,7 +99,7 @@ def list_manifest_versions(mani: dict[str, str], start_index: int, count: int) -
         result = result + keys[key_index] + '\n'
     
     return result
-        
+
         
 # def cache_response(res: requests.Response):
 #     cache = Path('.cache')
